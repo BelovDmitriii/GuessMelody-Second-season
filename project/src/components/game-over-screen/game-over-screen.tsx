561013123be5b9887b5f449493c0875bd7c7b89a
Belov Dmitrii <belovd90@mail.ru>
2023-02-04 19:13:43 +0300

    refactored game-ower component

--- project/src/components/game-over-screen/game-over-screen.tsx
+++ project/src/components/game-over-screen/game-over-screen.tsx
@@ -5,7 +5,7 @@ function GameOverScreen(): JSX.Element {
       <h2 className="result__title">Какая жалость!</h2>
       <p className="result__total result__total--fail">У вас закончились все попытки. Ничего, повезёт в следующий раз!</p>
       <button className="replay" type="button">Попробовать ещё раз</button>
-      </section>
+    </section>
   );
 }
 
