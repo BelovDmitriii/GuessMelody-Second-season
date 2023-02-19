d23a9416be0210c2e880ff97684028c89e36e432
Belov Dmitrii <belovd90@mail.ru>
2023-02-19 23:48:42 +0300

    added reset Game action

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,5 +1,5 @@
 import { createReducer } from '@reduxjs/toolkit/dist/createReducer';
-import { incrementStep } from './action';
+import { incrementStep, resetGame } from './action';
 import { FIRST_GAME_STEP } from '../consts/consts';
 
 const STEP_COUNT = 1;
@@ -13,7 +13,11 @@ const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(incrementStep, (state) => {
       state.step = state.step + STEP_COUNT;
-    });
+    })
+    .addCase(resetGame, (state) => {
+      state.step = FIRST_GAME_STEP,
+      state.mistakes = 0,
+  });
 });
 
 export {reducer};
