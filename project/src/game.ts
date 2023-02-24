b70ae8b46c2662ec5800eb304a662fb09bd5a08e
Belov Dmitrii <belovd90@mail.ru>
2023-02-24 12:57:48 +0300

    added functions for ckecking correct questions

---
+++ project/src/game.ts
@@ -0,0 +1,20 @@
+import { GameType } from './consts/consts';
+import { Question, UserAnswer, UserArtistQuestionAnswer, UserGenreQuestionAnswer, QuestionArtist, QuestionGenre } from './types/questions';
+
+export const isAnswerCorrect = (question: Question, answer: UserAnswer): boolean => {
+  if(question.type === GameType.Artist && typeof answer === 'string') {
+    return isArtistAnswerCorrect(question, answer);
+  }
+  if(question.type === GameType.Genre && Array.isArray(answer)) {
+    return isGenreAnswerCorrect(question, answer);
+  }
+
+  return false;
+};
+
+export const isArtistAnswerCorrect = (question: QuestionArtist, userAnswer: UserArtistQuestionAnswer):boolean =>
+  userAnswer === question.song.artist;
+
+export const isGenreAnswerCorrect = (question: QuestionGenre, userAnswer: UserGenreQuestionAnswer): boolean =>
+  userAnswer.every((answer, index) =>
+    answer === (question.answers[index].genre === question.genre));
