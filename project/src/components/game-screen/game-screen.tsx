dbb05f4102829900b28923dd9983e69bd898b8f9
Belov Dmitrii <belovd90@mail.ru>
2023-02-24 15:44:21 +0300

    added Navigate frome game-over screen

--- project/src/components/game-screen/game-screen.tsx
+++ project/src/components/game-screen/game-screen.tsx
@@ -4,7 +4,7 @@ import QuestionArtistScreen from '../question-artist-screen/question-artist-scre
 import QuestionGenreScreen from '../question-genre-screen/question-genre-screen';
 import withAudioPlayer from '../../hocks/with-audio-player/with-audio-player';
 import { useAppDispatch, useAppSelector } from '../../hooks';
-import { AppRoute, GameType } from '../../consts/consts';
+import { AppRoute, GameType, MAX_MISTAKE_COUNT } from '../../consts/consts';
 import { incrementStep, checkUserAnswer } from '../../store/action';
 import Mistakes from '../mistakes/mistakes';
 
@@ -20,6 +20,10 @@ function GameScreen(props:GameScreenProps): JSX.Element {
   const {step, mistakes} = useAppSelector((state) => state);
   const question = questions[step];
   const dispatch = useAppDispatch();
+
+  if(mistakes === MAX_MISTAKE_COUNT){
+    return <Navigate to={AppRoute.Lose} />;
+  }
 
   if(step >= questions.length || !question){
     return <Navigate to={AppRoute.Root} />;
--- project/src/consts/consts.tsx
+++ project/src/consts/consts.tsx
@@ -14,6 +14,7 @@ export enum AuthorizationStatus{
 }
 
 export const FIRST_GAME_STEP = 0;
+export const MAX_MISTAKE_COUNT = 3;
 
 export enum GameType {
   Genre = 'genre',
