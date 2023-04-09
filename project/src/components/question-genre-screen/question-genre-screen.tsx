ec8d9382d41b6b8cc80a272ecc82ca5e4545d8fe
Belov Dmitrii <belovd90@mail.ru>
2023-04-10 01:17:49 +0300

    added QuestionGenreItem component to QuestionGenreScreen

--- project/src/components/question-genre-screen/question-genre-screen.tsx
+++ project/src/components/question-genre-screen/question-genre-screen.tsx
@@ -1,7 +1,8 @@
 import Logo from '../logo/logo';
 import { QuestionGenre, UserGenreQuestionAnswer } from '../../types/questions';
-import { ChangeEvent, FormEvent, PropsWithChildren } from 'react';
+import { FormEvent, PropsWithChildren } from 'react';
 import { useUserAnswers } from '../../hooks/use-user-answer';
+import QuestionGenreItem from '../question-genre-item/question-genre-item';
 
 type QuestionGenreScreenProps = PropsWithChildren<{
   question: QuestionGenre;
@@ -40,20 +41,14 @@ function QuestionGenreScreen(props: QuestionGenreScreenProps):JSX.Element {
           {answers.map((answer, id) => {
             const keyValue = `${id}-${answer.src}`;
             return(
-              <div key={keyValue} className="track">
-                {renderPlayer(answer.src, id)}
-                <div className="game__answer">
-                  <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${id}`}
-                    id={`answer-${id}`}
-                    checked={userAnswers[id]}
-                    onChange={({target}: ChangeEvent<HTMLInputElement>) => {
-                      const value = target.checked;
-                      handleAnswerChange(id, value);
-                    }}
-                  />
-                  <label className="game__check" htmlFor={`answer-${id}`}>Отметить</label>
-                </div>
-              </div>
+              <QuestionGenreItem
+                answer={answer}
+                id={id}
+                key={keyValue}
+                onChange={handleAnswerChange}
+                renderPlayer={renderPlayer}
+                userAnswers={userAnswers[id]}
+              />
             );
           })}
 
