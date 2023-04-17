387f9fbdd46bcd3e9dac878b05665e74a0522885
Belov Dmitrii <belovd90@mail.ru>
2023-04-18 01:16:40 +0300

    added first test

--- project/src/game.test.ts
+++ project/src/game.test.ts
@@ -1,5 +1,12 @@
+import { isArtistAnswerCorrect } from './game';
+import { makeFakeQuestionArtist } from './utils/mocks';
+
+const mockArtistQuestion = makeFakeQuestionArtist();
+
 describe('Function': 'isArtistAnswerCorrect', () => {
   it('should return "true" when answer is correct', () => {
-    //Тестовый случай
+    const {artist: correctAnswer} = mockArtistQuestion.song;
+    expect(isArtistAnswerCorrect(mockArtistQuestion, correctAnswer))
+      .toBe(true);
   });
 });
