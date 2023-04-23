8bcb2f77541c980caef8a28b7f2f6d720e2a7075
Belov Dmitrii <belovd90@mail.ru>
2023-04-23 17:19:24 +0300

    added other tests to async operations

--- project/src/store/api-actions.test.ts
+++ project/src/store/api-actions.test.ts
@@ -5,12 +5,15 @@ import { State } from '../types/state';
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import {Action} from 'redux';
 import { APIRoute } from '../consts/consts';
-import { checkAuthAction } from './api-actions';
+import { checkAuthAction, loginAction, fetchQuestionAction, logoutAction } from './api-actions';
 import { requireAuthorization } from './user-process/user-process';
+import { AuthData } from '../types/auth-data';
+import { makeFakeQuestionArtist, makeFakeQuestionGenre } from '../utils/mocks';
+import { loadQuestions } from './game-data/game-data';
 
 describe('Async actions', () => {
   const api = createAPI();
-  const mockAPT = new MockAdapter(api);
+  const mockAPI = new MockAdapter(api);
   const middlewares = [thunk.withExtraArgument(api)];
 
   const mockStore = configureMockStore<
@@ -21,7 +24,7 @@ describe('Async actions', () => {
 
   it('должен вернуться статус авторизации "auth", когда сервер вернет 200', async ()=> {
     const store = mockStore();
-    mockAPT
+    mockAPI
       .onGet(APIRoute.Login)
       .reply(200, []);
 
@@ -33,5 +36,59 @@ describe('Async actions', () => {
     const actions = store.getActions().map(({type}) => type);
 
     expect(actions).toContain(requireAuthorization.toString());
+  });
+
+  it('должен вызвать RequiredAuthorization и RedirectToRoute, когда POST /login', async () => {
+    const fakeUser: AuthData = {login: 'testMail@test.ru', password: '1234566789'};
+
+    mockAPI
+      .onPost(APIRoute.Login)
+      .reply(200, {token: 'secret'});
+
+    const store = mockStore();
+    Storage.prototype.setItem = jest.fn();
+
+    await store.dispatch(loginAction(fakeUser));
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toContain(requireAuthorization.toString());
+
+    expect(Storage.prototype.setItem).toBeCalledTimes(1);
+    expect(Storage.prototype.setItem).toBeCalledWith('guess-melody-token', 'secret');
+  });
+
+  it ('должен вызвать Load_Questions, когда GET /questions', async () => {
+    const mockQuestions = [makeFakeQuestionArtist(), makeFakeQuestionGenre()];
+    mockAPI
+      .onGet(APIRoute.Questions)
+      .reply(200, mockQuestions);
+
+    const store = mockStore();
+    await store.dispatch(fetchQuestionAction());
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toContain(loadQuestions.toString());
+  });
+
+  it ('должен вызвать Logout, когда Delete /logout', async () => {
+    mockAPI
+      .onDelete(APIRoute.Logout)
+      .reply(204);
+
+    const store = mockStore();
+    Storage.prototype.removeItem = jest.fn();
+
+    await store.dispatch(logoutAction());
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toContain(requireAuthorization.toString());
+
+    expect(Storage.prototype.removeItem).toBeCalledTimes(1);
+    expect(Storage.prototype.removeItem).toBeCalledWith('guess-melody-token');
   });
 });
