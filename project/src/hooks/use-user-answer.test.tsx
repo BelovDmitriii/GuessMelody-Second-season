15f8c96a8025facdd412884f4febdb50979482a1
Belov Dmitrii <belovd90@mail.ru>
2023-05-01 15:52:15 +0300

    added test to hook

---
+++ project/src/hooks/use-user-answer.test.tsx
@@ -0,0 +1,41 @@
+import { renderHook, act } from '@testing-library/react';
+import { makeFakeQuestionGenre } from '../utils/mocks';
+import {useUserAnswers} from './use-user-answer';
+
+const fakeQuestionGenre = makeFakeQuestionGenre();
+
+describe('Hook: useUserAnswers', () => {
+  it('should return array with 3 elements', () => {
+    const {result} = renderHook(() =>
+      useUserAnswers(fakeQuestionGenre),
+    );
+
+    const [answers, handleAnswerChange] = result.current;
+
+    expect(result.current).toHaveLength(2);
+    expect(answers).toBeInstanceOf(Array);
+    expect(handleAnswerChange).toBeInstanceOf(Function);
+  });
+
+  it('должен правильно изаменить state', () => {
+    const expectedInitialAnswers = [false, false, false, false];
+
+    const {result} = renderHook(
+      () => useUserAnswers(fakeQuestionGenre),
+    );
+
+    const [initialAnswers] = result.current;
+    let[, handleAnswerChange] = result.current;
+
+    act(() => handleAnswerChange(1, true));
+    [,handleAnswerChange] = result.current;
+
+    act(() => handleAnswerChange(3, true));
+
+    const [answers] = result.current;
+
+    expect(initialAnswers).toStrictEqual(expectedInitialAnswers);
+    expect(answers[1]).toBe(true);
+    expect(answers[3]).toBe(true);
+  });
+});
